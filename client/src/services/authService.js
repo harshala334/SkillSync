a58b221fda6f5dbcb4794d65d9743f3f690eb01c
Harshala <mharshala334@gmail.com>
2025-08-19 15:05:23 +0530

    update authService and vercel configuration

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,6 @@
 export async function loginOrSignup(email, password, name) {
-  const endpoint = name ? '/api/auth/signup' : '/api/auth/login';
+  const apiBaseUrl = import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL || '';
+  const endpoint = name ? `${apiBaseUrl}/api/auth/signup` : `${apiBaseUrl}/api/auth/login`;
   const body = name ? { name, email, password } : { email, password };
   const res = await fetch(endpoint, {
     method: 'POST',
