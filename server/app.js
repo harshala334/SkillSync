b2a1c229fcd430aa733cd59e2a935a7eb50ad085
Harshala <mharshala334@gmail.com>
2025-08-19 23:29:08 +0530

    fix port

--- server/app.js
+++ server/app.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 // Middleware
 app.use(cors());
@@ -39,3 +41,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🚀`);
 });
+
+module.exports = app;
